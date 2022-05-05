f35a4e4bbae0d9edda5686b9fa7b3a6087c1ba9e
avinash-here <avinash.softer@gmail.com>
2022-05-05 11:14:28 +0530

    added navbar in about, brands and faqs page

--- information/associated.js
+++ information/associated.js
@@ -2,3 +2,7 @@
 import { footer2 } from '../components/footer.js';
 
 document.getElementById('footer_').innerHTML = footer2();
+
+import { navbar } from '../landing-page1/component/navbar.js';
+
+document.getElementById('navbar').innerHTML = navbar();
