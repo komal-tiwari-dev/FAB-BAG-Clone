18880b4a5916b7a7111e2d9cfabc48143f114bae
komal-tiwari-dev <atulzv1999@gmail.com>
2022-05-07 15:06:58 +0530

    final header footer commit

--- information/associated.js
+++ information/associated.js
@@ -3,6 +3,6 @@ import { footer2 } from '../components/footer.js';
 
 document.getElementById('footer_').innerHTML = footer2();
 
-import { navbar } from '../landing-page1/component/navbar.js';
+import { navbar } from '../components/navbar.js';
 
 document.getElementById('navbar').innerHTML = navbar();
